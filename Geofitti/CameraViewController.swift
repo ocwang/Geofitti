7f48c4540b59b6dbebfd88e724dd19c040352820
bryanye97 <bryanye97@gmail.com>
2016-06-25 15:23:48 -0700

    Added Camera VC and GrafittiVC

---
+++ Geofitti/CameraViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CameraViewController.swift
+//  Geofitti
+//
+//  Created by Chase Wang on 6/25/16.
+//  Copyright © 2016 Geofitti. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageViewTemporary: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+    @IBAction func openCamera(sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .Camera
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageViewTemporary.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true) {
+            self.performSegueWithIdentifier("editPhotoSegue", sender: self)
+        }
+        
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "editPhotoSegue" {
+            let destination = segue.destinationViewController as! GrafittiViewController
+            destination.image = imageViewTemporary.image
+        }
+        
+        
+        
+    }
+    
+        
+
+}
+
