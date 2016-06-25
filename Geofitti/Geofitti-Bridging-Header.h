0a58e81c53082240657efd7f11c9058109f0cf63
hakuna-matatas <alangao50@gmail.com>
2016-06-25 14:39:08 -0700

    Added objective-C bridging header

---
+++ Geofitti/Geofitti-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Geofitti-Bridging-Header.h
+//  Geofitti
+//
+//  Created by Alan Gao on 6/25/16.
+//  Copyright © 2016 Geofitti. All rights reserved.
+//
+
+#import "DrawView_BuildingPaths.m"
+#import <WILLCore/WILLCore.h>
